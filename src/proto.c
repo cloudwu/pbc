f7c3cdd27ef372e76dd5a0533153866baac89f17
Cloud Wu <cloudwu@gmail.com>
2012-02-10 21:45:39 +0800

    minor bug fix (pbc_type)

--- src/proto.c
+++ src/proto.c
@@ -240,7 +240,7 @@ pbc_type(struct pbc_env * p, const char * typename , const char * key , const ch
 		return 0;
 	}
 	if (key == NULL) {
-		return 1;
+		return 0;
 	}
 	struct _field * field = _pbcM_sp_query(m->name, key);
 	return _pbcP_type(field, type);
