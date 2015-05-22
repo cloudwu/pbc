f89bd871b9133548267fbede33ae0e1bd58a93cb
Cloud Wu <cloudwu@gmail.com>
2015-05-22 21:44:16 +0800

    add enum_id to lua53 binding and trigger Travis CI

--- binding/lua53/protobuf.lua
+++ binding/lua53/protobuf.lua
@@ -494,6 +494,10 @@ function M.register_file(filename)
 	f:close()
 end
 
+function enum_id(enum_type, enum_name)
+	return c._env_enum_id(P, enum_type, enum_name)
+end
+
 M.default=set_default
 
 return M
